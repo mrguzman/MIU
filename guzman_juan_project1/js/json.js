5988d587bd3214a0e57f6fade52a55d2154f4dbf
Juan Guzman <mrguzman83@fullsail.edu>
2013-02-08 16:16:06 -0800

    Created Dummy JSON

--- guzman_juan_project1/js/json.js
+++ guzman_juan_project1/js/json.js
@@ -31,5 +31,194 @@ var placeHolder = {
 		"interestLevel": ["Select Interest Level:", "2"],
 		"leadSource": ["Lead Source:", "Cold Call"],
 		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead4": {
+		"fname": ["First Name:", "John"],
+		"lname": ["Last Name", "Smith"],
+		"contactNum": ["Contact Number:", "4075551234"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2013-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "2"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead5": {
+		"fname": ["First Name:", "Jane"],
+		"lname": ["Last Name", "Smith"],
+		"contactNum": ["Contact Number:", "4075554321"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2013-12-11"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "2"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead6": {
+		"fname": ["First Name:", "Emiliana"],
+		"lname": ["Last Name", "Guzman"],
+		"contactNum": ["Contact Number:", "4075558959"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "2"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead7": {
+		"fname": ["First Name:", "John"],
+		"lname": ["Last Name", "Doe"],
+		"contactNum": ["Contact Number:", "4075555678"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "2"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead8": {
+		"fname": ["First Name:", "Jane"],
+		"lname": ["Last Name", "Doe"],
+		"contactNum": ["Contact Number:", "4075550986"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "2"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead9": {
+		"fname": ["First Name:", "Zalia"],
+		"lname": ["Last Name", "Guzman"],
+		"contactNum": ["Contact Number:", "4075552134"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "2"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead10": {
+		"fname": ["First Name:", "Lyka"],
+		"lname": ["Last Name", "Guzman"],
+		"contactNum": ["Contact Number:", "4075554312"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "5"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Dog"]
+ 	},
+ 		"lead11": {
+		"fname": ["First Name:", "Mickey"],
+		"lname": ["Last Name", "Guzman"],
+		"contactNum": ["Contact Number:", "4075550978"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "4"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead12": {
+		"fname": ["First Name:", "Blanca"],
+		"lname": ["Last Name", "Thurman"],
+		"contactNum": ["Contact Number:", "4075555687"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "7"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead13": {
+		"fname": ["First Name:", "Evan"],
+		"lname": ["Last Name", "Guzman"],
+		"contactNum": ["Contact Number:", "4075558959"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "2"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead14": {
+		"fname": ["First Name:", "Evan"],
+		"lname": ["Last Name", "Guzman"],
+		"contactNum": ["Contact Number:", "4075558959"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "2"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead15": {
+		"fname": ["First Name:", "Evan"],
+		"lname": ["Last Name", "Guzman"],
+		"contactNum": ["Contact Number:", "4075558959"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "2"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead16": {
+		"fname": ["First Name:", "Evan"],
+		"lname": ["Last Name", "Guzman"],
+		"contactNum": ["Contact Number:", "4075558959"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "2"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead17": {
+		"fname": ["First Name:", "Evan"],
+		"lname": ["Last Name", "Guzman"],
+		"contactNum": ["Contact Number:", "4075558959"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "2"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead18": {
+		"fname": ["First Name:", "Evan"],
+		"lname": ["Last Name", "Guzman"],
+		"contactNum": ["Contact Number:", "4075558959"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "2"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead19": {
+		"fname": ["First Name:", "Evan"],
+		"lname": ["Last Name", "Guzman"],
+		"contactNum": ["Contact Number:", "4075558959"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "2"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
+ 	},
+ 		"lead20": {
+		"fname": ["First Name:", "Evan"],
+		"lname": ["Last Name", "Guzman"],
+		"contactNum": ["Contact Number:", "4075558959"],
+		"contactType": ["Contact Type:", "Home"],
+		"date": ["Schedule Call Date:", "2012-12-21"],
+		"timeOfDay": ["Preferred Time:", "Morning"],
+		"interestLevel": ["Select Interest Level:", "2"],
+		"leadSource": ["Lead Source:", "Cold Call"],
+		"comments": ["Callback Commens:", "Son"]
  	}
+
+
 }
