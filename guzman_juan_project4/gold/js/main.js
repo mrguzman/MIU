f9980977b2c8d0d84aea1d88d35f95118a905afa
Juan Guzman <mrguzman83@fullsail.edu>
2013-02-23 22:06:52 -0800

    Troubleshooting.

--- guzman_juan_project4/gold/js/main.js
+++ guzman_juan_project4/gold/js/main.js
@@ -1,20 +1,48 @@
-
-var formValues = function(data){
-	console.log(data);
-};
-
-
-
+/*
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+	
+	
+});	
+*/
+		
 $(document).ready(function(){
-	
-	var scheduleForm = $('#scheduleCall');
-	
-	scheduleForm.validate({
-		invalideHandler: function(form, validator){},
-		submitHandler: function(){
-			var data = scheduleForm.serializeArray();
-			formValues(data);
+
+		var myForm = $('#scheduleCall');
+		    myForm.validate({
+			invalidHandler: function(form, validator) {
+			},
+			submitHandler: function() {
+		var data = myForm.serializeArray();
+			storeData(data);
 		}
 	});
 	
+	//any other code needed for addItem page goes here
+	
 });
+
+
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
+/*
+var autofillData = function (){
+	 
+};
+
+var getData = function(){
+
+};
+
+var storeData = function(data){
+	console.log(data);
+}; 
+
+var	deleteItem = function (){
+			
+};
+					
+var clearLocal = function(){
+
+};
+
+*/
