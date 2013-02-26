413f3ad843b05571553499921a2ca1d3f3f32015
Juan Guzman <mrguzman83@fullsail.edu>
2013-02-25 16:45:06 -0800

    Working to save to Local Storage

--- guzman_juan_project4/gold/js/main.js
+++ guzman_juan_project4/gold/js/main.js
@@ -13,9 +13,34 @@ $('#additem').on('pageinit', function(){
 			},
 			submitHandler: function() {
 		var data = myForm.serializeArray();
-			storeData(data);
+			storeData(this.key);
 		}
 	});
+	
+	var storeData = function(key){
+		console.log(key);
+	
+			if(!key){
+			var id = Math.floor(Math.random()*100000001);
+		}else{
+			var id = key;
+		}
+		var item = {};
+			item.fname = ["First Name:", $('#fname').value];
+			item.lname = ["Last Name:", $('#lname').value];
+			item.contactNum = ["Contact Number:", $('#contactNum').value];
+			item.contactType = ["Contact Type:", $('#contactType').value];
+			item.date = ["Scheduled Date:", $('#date').value];
+			item.timeOfDay = ["Preferred Time:", $('timeOfday').value];
+			item.interestLevel = ["Interest Level", $('#interestLevel').value];
+			item.leadSource = ["Lead Source:", $('#leadTypes').value];
+			item.comments = ["Comments:", $('#comments').value];
+			localStorage.setItem(id, JSON.stringify(item));
+			
+			alert("Callback Saved");
+
+}; 
+
 	
 	//any other code needed for addItem page goes here
 	
@@ -23,7 +48,7 @@ $('#additem').on('pageinit', function(){
 
 
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
-/*
+
 var autofillData = function (){
 	 
 };
@@ -32,9 +57,6 @@ var getData = function(){
 
 };
 
-var storeData = function(data){
-	console.log(data);
-}; 
 
 var	deleteItem = function (){
 			
@@ -44,4 +66,3 @@ var clearLocal = function(){
 
 };
 
-*/
